f28039a7229dc9255f7add3fa2fa7087436719d7
Gella3301 <ogorbunova249@gmail.com>
2025-01-23 20:43:42 +0500

    выполнена 5 задача 3 урока

---
+++ Task_03_05/Task_03_05/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_03_05
+{ 
+    using System;
+
+    class Program
+    {
+        static void Main()
+        { /* Написать программу, которая выводит на экран таблицу соответствия температуры в градусах Цельсия и
+Фаренгейта (F = C*1,8 + 32). Диапазон изменения температуры в градусах Цельсия и шаг должны вводиться во
+время работы программы*/
+
+
+            Console.WriteLine("Введите начальную температуру");
+            double t1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите конечную температуру");
+            double t2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите шаг изменения температуры");
+            double step = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Цельсия | Фаренгейт");
+            Console.WriteLine("-------------------");
+
+          
+            for (double celsius = t1; celsius <= t2; celsius += step)
+            {
+                double fahrenheit = celsius * 1.8 + 32;
+                Console.WriteLine($"{celsius}\t{fahrenheit}");
+            }
+        }
+    }
+}
+
