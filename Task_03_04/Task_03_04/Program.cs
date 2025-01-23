41377894649ffeb2d3a5e8f8f3e35208089ad23d
Gella3301 <ogorbunova249@gmail.com>
2025-01-23 20:40:44 +0500

    выполнена 4 задача 3 урока

---
+++ Task_03_04/Task_03_04/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_03_04
+{
+    using System;
+
+    public class TextCounter
+    {
+        public static void Main(string[] args)
+        { 
+            /*Пользователь вводить в консоли произвольный текст. После каждого ввода консоль очищается. Когда
+пользователь вводить слово «exit» или пустую строку – ввод останавливается и выводиться количество строк
+введенных пользователем.*/
+
+            int count = 0;
+            string input;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.Write("Введите текст (или enter /exit): ");
+                input = Console.ReadLine(); 
+
+                if (string.IsNullOrEmpty(input) || input == "exit")
+                {
+                    break; 
+                }
+
+                count++;
+            }
+
+            Console.WriteLine($"\nВы ввели {count} строк.");
+        }
+    }
+}
