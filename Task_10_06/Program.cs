c922fc238d088bbce8b1bac66821573313a4c363
Gella3301 <ogorbunova249@gmail.com>
2025-03-05 22:09:33 +0500

    задание 6 урока 10 выполнено

---
+++ Task_10_06/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Task_10_06
+{
+    internal class Program
+    { /*Создать Метод ArrayGeneration не возвращающий значения, принимает целое число n, выводит на консоль сгенерированный массив размерности n*n.*/
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число n");
+            int n = int.Parse(Console.ReadLine());
+            ArrayGeneration(n);
+            //Console.WriteLine(ArrayGeneration(n));
+        }
+        static void ArrayGeneration(int n)
+        { 
+            int [,] array = new int[n, n];
+            Random rnd = new Random();
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                { 
+                    array[i, j] = rnd.Next(1,100);
+                }
+            }
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
