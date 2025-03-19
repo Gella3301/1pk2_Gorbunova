79edec941f14604e900fba7c999e540a95720d3c
Gella3301 <ogorbunova249@gmail.com>
2025-03-19 20:35:49 +0500

    задача 4 урока 14 выполнена

---
+++ Task_14_04/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task_14_04
+{ /*Определите класс User, который будет иметь статическое свойство CurrentUser, представляющее текущего пользователя, и метод для его установки.*/
+    internal class Program
+    {
+        public class User
+        {
+            public int Id;
+            public string Name;
+
+           
+            public static User CurrentUser;
+
+            public User(int id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+            public static void SetCurrentUser(User user)
+            {
+                CurrentUser = user;
+            }
+
+        
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"ID: {Id}, Имя: {Name}");
+            }
+        }
+
+            static void Main(string[] args)
+            {
+              
+                User user1 = new User(11037, "Эми");
+                
+
+             
+                User.SetCurrentUser(user1);
+
+                Console.WriteLine("Текущий пользователь:");
+                User.CurrentUser.DisplayInfo();
+
+              
+            }
+        
+    }
+}
