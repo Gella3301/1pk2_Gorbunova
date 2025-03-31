05b8b1d4d70cf990ad6f0d47781548b7a97e980b
Gella3301 <ogorbunova249@gmail.com>
2025-03-31 21:36:11 +0500

    задание 4 урока 20 выполнено

---
+++ Task_20_04/Program.cs
@@ -0,0 +1,65 @@
+﻿
+    namespace Task_20_04
+    { /*Создайте программу для учета транспортных средств. Используйте перечисление VehicleType:
+• Car
+• Bike
+• Bus
+• Truck
+• Motorcycle
+Храните список транспортных средств (можно просто List<VehicleType>). Добавьте метод для подсчёта транспорта определённого типа (например, сколько грузовиков). Реализуйте поиск по типу и вывод информации.*/
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                List<VehicleType> vehicles = new List<VehicleType>
+            {
+                VehicleType.Car,
+                VehicleType.Car,
+                VehicleType.Car,
+                VehicleType.Bike,
+                VehicleType.Bus,
+                VehicleType.Bus,
+                VehicleType.Bus,
+                VehicleType.Bus,
+                VehicleType.Motorcycle,
+                VehicleType.Truck,
+                VehicleType.Truck
+
+            };
+                Console.WriteLine("Все транспортные средства:");
+                PrintVehicles(vehicles);
+
+
+                Console.WriteLine("\nКоличество транспортных средств по типам:");
+                foreach (VehicleType type in Enum.GetValues(typeof(VehicleType)))
+                {
+                    int count = CountVehiclesByType(vehicles, type);
+                    Console.WriteLine($"{type}: {count}");
+                }
+                static void PrintVehicles(List<VehicleType> vehicles)
+                {
+                    if (vehicles.Count == 0)
+                    {
+                        Console.WriteLine("Транспортные средства не найдены.");
+                        return;
+                    }
+
+                    for (int i = 0; i < vehicles.Count; i++)
+                    {
+                        Console.WriteLine($"{i + 1}. {vehicles[i]}");
+                    }
+                }
+                static int CountVehiclesByType(List<VehicleType> vehicles, VehicleType type)
+                {
+                    int count = 0;
+                    foreach (var vehicle in vehicles)
+                    {
+                        if (vehicle == type) count++;
+                    }
+                    return count;
+                }
+            }
+        }
+    }
+
+
