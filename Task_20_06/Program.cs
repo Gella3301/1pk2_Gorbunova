4954a9f1a8083842bf22a8e12e2ca305a7ffbc48
Gella3301 <ogorbunova249@gmail.com>
2025-03-31 22:28:40 +0500

    задание 6 урока 20 выполнено

---
+++ Task_20_06/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+
+namespace Task_20_06
+{
+    internal class Program
+    { /*Создайте программу, имитирующую работу светофора, используя перечисление TrafficLightColor:
+• Red
+• Yellow
+• Green
+Реализуйте автоматическое переключение цветов (каждые 3 секунды). При смене цвета выводите его в консоль (можно с задержкой Thread.Sleep). Добавьте возможность ручного переключения (например, по нажатию клавиши).*/
+        static void Main(string[] args)
+
+        {
+            Color light = Color.Red;
+
+            while (true)
+            {
+                
+                Console.Clear();
+                Console.ForegroundColor = light switch
+                {
+                    Color.Red => ConsoleColor.Red,
+                    Color.Yellow => ConsoleColor.Yellow,
+                    _ => ConsoleColor.Green
+                };
+                Console.WriteLine($"Светофор: {light}");
+                Console.ResetColor();
+
+              
+                if (Console.KeyAvailable)
+                {
+                    Console.ReadKey(true); 
+                }
+                else
+                {
+                    Thread.Sleep(3000);
+                }
+
+               
+                light = light switch
+                {
+                    Color.Red => Color.Yellow,
+                    Color.Yellow => Color.Green,_
+                     => Color.Red
+                };
+            }
+        }
+    }
+
+
+
+
+}
+
+
+
