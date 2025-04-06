8e31a33668f4fa5283035d6ae39d1b30c37ac7df
Gella3301 <ogorbunova249@gmail.com>
2025-04-06 19:03:07 +0500

    задание 9 урока 21 выполнено

---
+++ Task_21_09/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Task_21_09
+{ /*создайте класс билета (Ticket). сгенерируйте список из 30 билетов. произведите:
+• поиск билета с максимальной суммой,
+• билета с минимальной суммой,
+• выведите список билетов с багажом
+• выведите список люготных билетов*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+            var tic = new List<Ticket>();
+
+            for (int i = 0; i < 30; i++)
+                tic.Add(new Ticket
+                {
+                    Price = rnd.Next(1000, 10001),
+                    HasLuggage = rnd.Next(2) == 0,
+                    IsDiscount = rnd.Next(4) == 0
+                });
+
+           
+            Console.WriteLine($"Mаксимальная цена: {tic.Max(x => x.Price)} Mинимальная цена: {tic.Min(x => x.Price)}");
+
+            Console.WriteLine("\nСписок билетов с  багажом:");
+            tic.FindAll(x => x.HasLuggage).ForEach(x => Console.WriteLine(x.Price));
+
+            Console.WriteLine("\nЛьготные билеты:");
+            tic.FindAll(x => x.IsDiscount).ForEach(x => Console.WriteLine(x.Price));
+        }
+    }
+    
+}
---
+++ Task_21_09/Ticket.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_21_09
+{
+    public class Ticket
+    {
+        public decimal Price { get; set; }
+        public bool HasLuggage { get; set; }
+        public bool IsDiscount { get; set; }
+
+    }
+}
