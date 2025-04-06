7905179390788dd217c491669f2ffc3a61e4b557
Gella3301 <ogorbunova249@gmail.com>
2025-04-06 17:03:26 +0500

    задание 6 урока 21 выполнено

---
+++ Task_21_06/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task_21_06
+{
+    internal class Program
+    { /*Написать метод, который удаляет повторяющиеся элементы из списка, сохраняя порядок*/
+        static void Main(string[] args)
+        {
+            var items = new List<int> { 1, 2, 2, 3, 1, 2, 4, 7, 1 };
+            var uniqueItems = RemoveDuplicates(items);
+
+            foreach (var num in uniqueItems)
+                Console.Write(num + " "); 
+
+            Console.WriteLine(); 
+        }
+        
+     
+        public static List<int> RemoveDuplicates(List<int> list)
+        {
+            var unique = new HashSet<int>();
+            var result = new List<int>();
+
+            foreach (var item in list)
+                if (unique.Add(item))
+                    result.Add(item);
+            return result;
+        }
+    }
+}
