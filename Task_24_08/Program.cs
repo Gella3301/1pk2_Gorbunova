f74223f40b0418f8ad2a04cf0c46ede16974a327
Gella3301 <ogorbunova249@gmail.com>
2025-04-11 23:02:55 +0500

    задание 8 урока 24 выполнено

---
+++ Task_24_08/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Task_24_08
+{
+    internal class Program
+    { /*реализуйте функцию, осуществляющую поиск текста в файле и его замену на значения, введенные пользователем*/
+        static void Main(string[] args)
+        {
+            string filePath = "text.txt";
+
+            Console.WriteLine("Введите текст для поиска:");
+            string searchText = Console.ReadLine();
+           
+
+            Console.WriteLine("Введите текст для замены:");
+            string replaceText = Console.ReadLine();
+            
+
+            ReplaceInFile(filePath, searchText, replaceText);
+
+            Console.WriteLine("Замена выполнена успешно.");
+            Console.ReadKey();
+
+        }
+        static void ReplaceInFile(string filePath, string searchText, string replaceText)
+        {
+
+           
+            string fileContent = File.ReadAllText(filePath);
+
+            
+            fileContent = fileContent.Replace(searchText, replaceText, StringComparison.OrdinalIgnoreCase);
+
+            File.WriteAllText(filePath, fileContent);
+        }
+    }
+}
