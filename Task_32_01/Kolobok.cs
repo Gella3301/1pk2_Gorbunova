6b5d06d3def4ede8e69f743fa3006884734fffa9
Gella3301 <ogorbunova249@gmail.com>
2025-04-29 11:19:39 +0500

    задача 1 урока 32 выполнена

---
+++ Task_32_01/Kolobok.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Task_32_01
+{
+    public class Kolobok
+    {
+        public int Position { get; private set; }
+        public int Speed { get; }
+        public bool IsAlive { get; private set; }
+        public bool CanMove { get; private set; }
+        public Kolobok()
+        {
+            Position = 0;
+            Speed = 1;
+            IsAlive = true;
+            CanMove = true;
+        }
+
+        public void Roll()
+        {
+            Position += Speed;
+            Console.WriteLine($"Колобок катится, текущая позиция: {Position}");
+        }
+        public void MeetAnimal(Animal animal)
+        {
+            if (animal.CanEat(this))
+            {
+                IsAlive = false;
+                Console.WriteLine($"Колобок был съеден {animal.Name}!");
+            }
+            else
+            {
+                Console.WriteLine($"Колобок убежал от {animal.Name}!");
+            }
+        }
+        public void MeetObstacle(Obstacle obstacle)
+        {
+            if (obstacle.CanStop(this))
+            {
+                CanMove = false;
+                Console.WriteLine($"Колобок остановился у {obstacle.Name}!");
+            }
+            else
+            {
+                Console.WriteLine($"Колобок преодолел {obstacle.Name} катится дальше !");
+            }
+        }
+
+        public class Hare : Animal
+        {
+            public Hare() : base("Заяц") { }
+            public override bool CanEat(Kolobok kolobok)
+            {
+                return false;
+            }
+        }
+        public class Rock : Obstacle
+        {
+            public Rock() : base("Камень") { }
+            public override bool CanStop(Kolobok kolobok)
+            {
+              
+                return false; 
+            }
+        }
+        public class Wolf : Animal
+        {
+            public Wolf() : base("Волк") { }
+            public override bool CanEat(Kolobok kolobok)
+            {
+                return false;
+            }
+        }
+        public class Bear : Animal
+        {
+            public Bear() : base("Медведь") { }
+            public override bool CanEat(Kolobok kolobok)
+            {
+                return false;
+            }
+        }
+        public class Abyss : Obstacle
+        {
+            public Abyss() : base("Пропасть") { }
+            public override bool CanStop(Kolobok kolobok)
+            {
+                return true;
+            }
+        }
+
+            public class Fox : Animal
+        {
+            public Fox() : base("Лиса") { }
+            public override bool CanEat(Kolobok kolobok)
+            {
+                return true;
+            }
+        }
+      
+    }
+}
---
+++ Task_32_01/Program.cs
@@ -0,0 +1,51 @@
+﻿using static Task_32_01.Kolobok;
+
+namespace Task_32_01
+{
+    internal class Program
+  { 
+        static void Main(string[] args)
+        {
+            Kolobok kolobok = new Kolobok();
+            List<Animal> animals = new List<Animal>
+{
+new Hare(),
+new Wolf(),
+new Bear(),
+new Fox()
+};
+            List<Obstacle> obstacle = new List<Obstacle>
+{
+new Rock(),
+new Abyss(),
+
+};
+            foreach (var animal in animals)
+            {
+                if (!kolobok.IsAlive)
+                    break;
+                kolobok.Roll();
+                kolobok.MeetAnimal(animal);
+                foreach (var obstacles in obstacle)
+                {
+                    if (!kolobok.CanMove)
+                    break;
+                    kolobok.Roll();
+                    kolobok.MeetObstacle(obstacles);
+                       
+                }
+            }
+          if (kolobok.IsAlive )
+            {
+                Console.WriteLine("Колобок сбежал и стал Senior .NET-разработчиком!");
+            }
+            else
+            {
+                Console.WriteLine("Game Over. Колобок не выжил в этом жестоком ООП-мире.");
+                
+            }
+
+        }
+    }
+}
+
